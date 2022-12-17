4bcd2fa8360d2aa526aeea3dcd26fc3d17c446ec
菘菘 <rrt467778@gmail.com>
2022-12-17 18:48:32 +0800

    改善連線最佳伺服器的方式

--- src/main/kotlin/tw/brilliant/server/plugins/connectbestserver/event/BungeeEventListener.kt
+++ src/main/kotlin/tw/brilliant/server/plugins/connectbestserver/event/BungeeEventListener.kt
@@ -1,22 +1,19 @@
 package tw.brilliant.server.plugins.connectbestserver.event
 
-import tw.brilliant.server.plugins.connectbestserver.ConnectBestServer
-import tw.brilliant.server.plugins.connectbestserver.storage.BestServerSort.LeastPlayers
-import tw.brilliant.server.plugins.connectbestserver.storage.BestServerSort.MostPlayers
-import tw.brilliant.server.plugins.connectbestserver.storage.PluginConfig
 import net.md_5.bungee.api.ProxyServer
 import net.md_5.bungee.api.config.ServerInfo
-import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.event.ServerConnectEvent
 import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
 import net.md_5.bungee.module.reconnect.yaml.YamlReconnectHandler
-import java.util.logging.Level
+import tw.brilliant.server.plugins.connectbestserver.storage.BestServerSort.LeastPlayers
+import tw.brilliant.server.plugins.connectbestserver.storage.BestServerSort.MostPlayers
+import tw.brilliant.server.plugins.connectbestserver.storage.PluginConfig
 
 class BungeeEventListener : Listener {
     @EventHandler
-    fun onPostLogin(event: ServerConnectEvent) {
-        if (event.reason != ServerConnectEvent.Reason.JOIN_PROXY) return
+    fun onServerConnect(event: ServerConnectEvent) {
+        if (event.reason != ServerConnectEvent.Reason.JOIN_PROXY || event.isCancelled) return
 
         val handler = ProxyServer.getInstance().reconnectHandler
         var isFirstJoin = false
@@ -31,7 +28,6 @@ class BungeeEventListener : Listener {
             }
         }
 
-        val player = event.player
         if (isFirstJoin) {
             val servers =
                 ProxyServer.getInstance().servers.filter { !PluginConfig.blacklistServers.contains(it.value.name) }
@@ -41,24 +37,7 @@ class BungeeEventListener : Listener {
                 LeastPlayers -> servers.minBy { it.value.players.size }.value
             }
 
-            // If the player is already at the best server, don't move them
-            if (event.target.name != server.name) {
-                val thread = Thread(SetServer(server, player), "set-best-server")
-                thread.start()
-            }
-        }
-    }
-}
-
-private class SetServer(private val server: ServerInfo, private val player: ProxiedPlayer) : Runnable {
-    override fun run() {
-        try {
-            player.connect(server)
-            /// Waiting connect the player to the server.
-            Thread.sleep(500)
-            ProxyServer.getInstance().reconnectHandler.setServer(player)
-        } catch (e: Exception) {
-            ConnectBestServer.LOGGER.log(Level.SEVERE, "Failed to set the best server to player", e)
+            event.target = server
         }
     }
 }
